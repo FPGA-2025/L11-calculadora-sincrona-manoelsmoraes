0eb726653b5714f1d5651bc01bb9150f10813634
manoelsmoraes <63382033+manoelsmoraes@users.noreply.github.com>
2025-04-29 20:36:14 -0300

    Update calculadora_sincrona.v
    Teste ok

--- calculadora_sincrona.v
+++ calculadora_sincrona.v
@@ -1,9 +1,44 @@
 module calculadora_sincrona(
+    input clk,
+    input rst,
     input [7:0] entrada,
     input [2:0] codigo,
     output reg [7:0] saida
 );
     
 //insira seu código aqui
+
+
+        reg [7:0] acumulador;
+
+        always @(posedge clk or posedge rst) begin
+                if (rst) begin
+                        acumulador <= 8'b0;
+                        saida <= 8'b0;
+                        // bloco acima reset
+                end else begin
+
+                        case (codigo)
+                                3'b000: begin
+                                        saida <= entrada;  //Mostra entrada
+                                end
+                                3'b001: begin
+                                        acumulador <= acumulador + entrada; // somar
+                                        saida <= 8'b0;
+                                end
+                                3'b010: begin
+                                        acumulador <= acumulador - entrada; // subtrair
+                                        saida <= 8'b0;
+                                end
+                                3'b011: begin
+                                        saida <= acumulador;  //Mostra acumulador
+                                end
+                                default: begin
+                                        saida <= 8'b0;  // Codigos invalidos
+                                end
+                        endcase
+                end
+
+           end
 
 endmodule
